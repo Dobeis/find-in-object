83f79d920838dfc1e5ad8c7fc2f13a63df4cb540
Heitor Dobeis <heitor.dobeis@tecnospeed.com.br>
2019-03-20 13:50:09 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const findObjectKey = (object, key) => {
+  let value
+  Object.keys(object).some(function (k) {
+    if (k === key) {
+      value = object[k]
+      return true
+    }
+    if (object[k] && typeof object[k] === 'object') {
+      value = findObjectKey(object[k], key)
+      return value !== undefined
+    }
+  })
+  return value
+}
+
+module.exports = findObjectKey
+
